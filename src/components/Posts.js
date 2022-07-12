c6940f9cd055c7ab237c9945e23bbe463e085a25
unknown <ojeksdarey@gmail.com>
2022-07-12 20:20:21 +0000

    initial commit

---
+++ src/components/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Posts = ({ posts, loading }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <ul className='list-group mb-4'>
+      {posts.map(post => (
+        <li key={post.id} className='list-group-item'>
+         <h6>{post.title}</h6> 
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Posts;
